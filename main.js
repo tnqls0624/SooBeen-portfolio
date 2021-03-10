3d249279c4fa916237392c3fdc092b5056881951
tnqls0624 <56984613+tnqls0624@users.noreply.github.com>
2021-03-10 14:03:35 +0900

    JS : navbar menu click

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ document.addEventListener("scroll", () => {
     navbar.classList.remove("navbar--dark");
   }
 });
+
+// Handle Scrolling when tapping on the navbar
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+});
